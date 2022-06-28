30bc1c3e2e92a121bb7bc9b59e0a84bc2a0aa127
Jakub Jachowski <>
2022-06-28 20:41:18 +0200

    added list of things I want to add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
-    <div className='flex justify-center'>
-      <h1 className='text-3xl font-bold p-8 bg-purple-300 rounded-2xl shadow-2xl'>
+    <div className='flex justify-center flex-col p-4'>
+      <h1 className='text-3xl font-bold p-8 '>
         {`hello, I'm Jakub and this will be my portfolio!`}
       </h1>
+      <div className='mt-8 bg-green-300 p-8 rounded-2xl shadow-2xl'>
+        <h2 className='text-2xl font-bold list-disc'>
+          list of cool things I want to try/add:
+        </h2>
+        <ul className='mt-4 list-disc text-lg'>
+          <li>
+            <s>tailwind</s>
+          </li>
+          <li>make this page not look like trash</li>
+          <li>zod - object validation with cool typescript features</li>
+          <li>next.js backend</li>
+          <li>react query</li>
+          <li>tRCP - typesafe api</li>
+        </ul>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    listStyleType: {
+      disc: 'disc'
+    }
   },
   plugins: [],
 }

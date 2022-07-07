2883e38039e13f59a0e3e1d2d4ce7fddd812d1d3
Jakub Jachowski <>
2022-07-07 20:49:09 +0200

    added super cool background 🚀

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,15 @@ import type { NextPage } from 'next';
 import { Card } from '../components/Card';
 import { TodoList } from '../components/TodoList';
 
+// background-color: #;
+// opacity: 0.8;
+// background-image:  radial-gradient(#444cf7 0.5px, transparent 0.5px), radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px);
+// background-size: 20px 20px;
+// background-position: 0 0,10px 10px;
+// background-color: #e5e5f7;
+// opacity: 1;
+// background-image: radial-gradient(#b88ef2 2px, #e5e5f7 2px);
+// background-size: 40px 40px;
 const Home: NextPage = () => {
   return (
     <div className='flex justify-center flex-col p-4'>

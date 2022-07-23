4d14bb2e1cff7e9914dc7d666b0cae66a764e710
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-23 13:18:43 +0200

    moved code sample to separate file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,16 @@ import { TodoList } from '../components/TodoList';
 import { blogTodo } from '../data/blogCreationTodo';
 import { thingsToTry } from '../data/thingsToTryList';
 import { todoList } from '../data/todoList';
+import { codeSample } from '../data/codeSampleText';
 
 const Home: NextPage = () => {
-  const text = `<Layout>
-  <Header />
-  <CommitStreak />
-  <div className='flex flex-col space-y-4'>
-    <code>
-      <SyntaxHighlighter language='typescript'>{text}</SyntaxHighlighter>
-    </code>
-    <TodoList
-      items={blogTodo}
-      title='next cool feature - blog posts'
-      superCool
-    />
-    <TodoList
-      items={thingsToTry}
-      title='list of cool things I wanna try!'
-    />
-    <TodoList items={todoList} title='todo list on this page' />
-  </div>
-</Layout>`;
+  
   return (
     <Layout>
       <Header />
       <CommitStreak />
       <div className='flex flex-col space-y-4'>
-        <Code>{text}</Code>
+        <Code>{codeSample}</Code>
         <TodoList
           items={blogTodo}
           title='next cool feature - blog posts'

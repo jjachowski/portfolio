464b19f6f1bbe3b3ee62cb665f28fdc1bbb59da6
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-24 13:15:13 +0200

    improved displaying logged user email

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,13 +2,13 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 export const Login: React.FC = () => {
-  const { data, status } = useSession();
+  const { data } = useSession();
   return (
     <div className="h-4 mb-4 p-2 flex flex-row justify-end text-gray-600">
       {data?.user?.name}{" "}
       {data ? (
         <>
-          hello {data.user?.email}!{" "}
+          hello {data.user?.email?.split("@")[0]}!{" "}
           <p
             className="cursor-pointer underline ml-4"
             onClick={() => signOut()}

1a279a1c27e7a3380428d5ac52ec2cd11e34066e
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-24 13:07:23 +0200

    added cool icon and new photo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/future/image';
-import React from 'react';
-import { GithubLink } from './GithubLink';
+import Image from "next/future/image";
+import React from "react";
+import { GithubLink } from "./GithubLink";
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
-    <div className=' flex flex-row items-center my-4'>
+    <div className=" flex flex-row items-center my-4">
       <Image
-        src={'/myself.jpg'}
-        className='w-28 rounded-full shadow-2xl mr-4 b border-solid border-4 border-pink-900'
-        alt='Picture of a really swell guy, author of this website, magnificent beard and slick hairstyle'
+        src={"/myself.jpeg"}
+        className="w-28 rounded-3xl shadow-2xl mr-4 b border-solid border-4 border-black"
+        alt="Picture of a really swell guy, author of this website, magnificent beard and slick hairstyle, driving honda civic, wearing sunglasses and baseball cap"
       />
       <div>
-        <h1 className='text-xl sm:text-2xl  font-bold mb-1'>
+        <h1 className="text-xl sm:text-2xl  font-bold mb-1">
           {`Hi, I'm Jakub and this is my portfolio website`}
         </h1>
         <GithubLink />

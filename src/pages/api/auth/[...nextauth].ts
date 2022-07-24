cfce77b0bb4d208db02863f92a40531f84f6ce09
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-24 12:24:22 +0200

    displaying authenticated user and possibility to log in/out 😍

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,15 +3,13 @@ import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { prisma } from '../../../server/db/client';
 
-const THIRTY_DAYS = 30 * 24 * 60 * 60
-const THIRTY_MINUTES = 30 * 60
-
 export default NextAuth({
   // Configure one or more authentication providers
+  secret: process.env.SECRET,
   session: {
-    strategy: 'jwt',
-    maxAge: THIRTY_DAYS,
-    updateAge: THIRTY_MINUTES
+    strategy: "database",
+    maxAge: 365 * 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
   },
   adapter: PrismaAdapter(prisma),
   providers: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,23 +9,15 @@ import { thingsToTry } from '../data/thingsToTryList';
 import { todoList } from '../data/todoList';
 import { codeSample } from '../data/codeSampleText';
 import { useSession } from 'next-auth/react';
+import { Login } from '../components/Login';
 
 const Home: NextPage = () => {
-  const { data, status } = useSession()
   return (
     <Layout>
-      <div className='absolute right-2 top-2 bg-purple-300 rounded-md px-2 w-50 overflow-hidden whitespace-nowrap transition-all duration-1000 w-10 hover:w-60'>logged in as: {data?.user?.email}</div>
-      <div>
-      </div>
+      <Login/>
       <Header />
       <CommitStreak />
       <div className='flex flex-col space-y-4'>
-        <div className='bg-blue-400 h-40'>
-          {JSON.stringify(data)}
-        </div>
-        <div className='bg-purple-400 h-40'>
-          {JSON.stringify(status)}
-        </div>
         <Code>{codeSample}</Code>
         <TodoList
           items={blogTodo}

bfcd5e2ecfae83d4e3e5017d090ac9fa6620120e
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-25 21:16:35 +0200

    changed db to planet scale

--- prisma/migrations/20220714163329_post_title_added/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `title` to the `blogposts` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "blogposts" ADD COLUMN     "title" TEXT NOT NULL;

26f5d312af5ed0eaa2c3109077b507bb37559971
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-27 21:04:18 +0200

    added link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -7,6 +7,7 @@ export const Links: React.FC<LinksProps> = () => {
   return (
     <div className='my-4 flex flex-col space-y-1'>
       <Link href='/snippets' text='snippets I use' />
+      <Link href='/blog/add' text='work in progress' />
     </div>
   );
 };

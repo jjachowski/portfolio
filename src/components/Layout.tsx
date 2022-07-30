595f01c0d953f5d3ef00845fc844ce63283c25b5
Jakub Jachowski <jakubjachowski@icloud.com>
2022-07-30 12:38:16 +0200

    added bottom margin to layout 🥸

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   const { asPath } = useRouter();
   return (
-    <div className='w-screen flex justify-center '>
+    <div className='w-screen flex justify-center mb-16'>
       {asPath !== '/' && <BackToHomepage />}
       <div className='flex flex-col w-full px-3 sm:w-3/4 lg:w-3/5 max-w-5xl'>
         {children}
